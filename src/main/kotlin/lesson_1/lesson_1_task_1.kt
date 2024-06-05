1a5af3b2a8a80e4216f99154910b07db76d890ce
Sugguttor <antonofmixa@mail.ru>
2024-06-05 19:13:46 +0300

    KS-1-1 print data from first lesson

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -1,8 +1,7 @@
 package lesson_1
 
 fun main() {
-
-    val numberOfOrders :Int = 75
-    val thanksForThePurchase : String = "Спасибо за покупку!"
+    val numberOfOrders: Int = 75
+    val thanksForThePurchase: String = "Спасибо за покупку!"
     println("Это решение задачи 1 из урока 1")
 }
