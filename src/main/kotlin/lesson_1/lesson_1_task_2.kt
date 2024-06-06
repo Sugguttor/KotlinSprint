39fd8a02d9a316f15feb8190ebebfbbcff8f132b
Sugguttor <antonofmixa@mail.ru>
2024-06-06 18:30:15 +0300

    KS-1-2 reduced number of employees

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,12 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thanksForThePurchase: String = "Спасибо за покупку!"
+    println(numberOfOrders)
+    println(thanksForThePurchase)
+    var numberOfEmployees: Int = 2000
+//    println(numberOfEmployees)
+    numberOfEmployees = numberOfEmployees - 1
+    println(numberOfEmployees)
+}
