7d72d2061f3cdf0e27fe11e97af5199473b84230
Sugguttor <antonofmixa@mail.ru>
2024-06-07 19:59:14 +0300

    KS-1-5 Time conversion

--- src/main/kotlin/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1_task_5.kt
@@ -1,3 +1,6 @@
 fun main() {
-
+    val seconds: Int = 6480
+    val minutes: Int = seconds / 60
+    val hour: Int = minutes / 60
+    println("0$hour:${minutes-(hour*60)}:0${seconds-(minutes*60)}")
 }
