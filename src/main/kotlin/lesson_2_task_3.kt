5ff616a819f5144a36b368b9f47d128b8ef9053f
Sugguttor <antonofmixa@mail.ru>
2024-06-12 11:30:46 +0300

    KS-2-2

---
+++ src/main/kotlin/lesson_2_task_3.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val departureTime = "9:39"
+    val travelTime = 457
+
+    val time = departureTime.split(:)
+}
+//val second = 6480
+//val minutes: Int = second / SEC
+//val hours: Int = minutes / SEC
+//val transferOfMinutes = minutes - (hours * SEC)
+//val transferOfSeconds = second - (minutes * SEC)
+//println("%02d:%02d:%02d".format(hours, transferOfMinutes, transferOfSeconds))
+
+const val SEC = 60
