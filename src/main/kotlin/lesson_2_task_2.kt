1c96378e870b0968669138999a615c674fe8e51a
Sugguttor <antonofmixa@mail.ru>
2024-06-12 11:14:01 +0300

    KS-2-2

--- src/main/kotlin/lesson_2_task_2.kt
+++ src/main/kotlin/lesson_2_task_2.kt
@@ -1,16 +1,17 @@
 fun main() {
     val permanentEmployees: Int = 50
-    println("Число постоянных работнико - $permanentEmployees")
     val salaryOfPermanentEmployees: Int = 30000
-    println("Зарплата постоянного работника - $salaryOfPermanentEmployees")
     val intern: Int = 30
-    println("Число стажеров - $intern")
     val salaryOfInterns: Int = 20000
-    println("Зарплата стажера - $salaryOfInterns")
     val costOfPermanent: Int = permanentEmployees * salaryOfPermanentEmployees
     val costOfInterns: Int = intern * salaryOfInterns
     val generalExpenses: Int = costOfInterns + costOfPermanent
     val totalNumberOfEmployees = permanentEmployees + intern
     val averageSalary = generalExpenses / totalNumberOfEmployees
+
+    println("Число постоянных работнико - $permanentEmployees")
+    println("Зарплата постоянного работника - $salaryOfPermanentEmployees")
+    println("Число стажеров - $intern")
+    println("Зарплата стажера - $salaryOfInterns")
     println("Средняя зарплата сотрудника - $averageSalary")
 }
