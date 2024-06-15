0d91d8e3115e58fc33f154f668e301ccad907b8e
Sugguttor <antonofmixa@mail.ru>
2024-06-15 14:43:34 +0300

    KS-3-1

---
+++ src/main/kotlin/lesson_3_task_1.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val name = "Игнат"
+    val hallow = "Добрый"
+    val timeOfDay = "день"
+    val timeOfDay1 = "вечер"
+
+    var morningGreeting = "$hallow $timeOfDay, $name!"
+    println(morningGreeting)
+    morningGreeting = "$hallow $timeOfDay1, $name!"
+    println(morningGreeting)
+}
