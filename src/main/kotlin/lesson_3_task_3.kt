749e49c1de5ff582f4214eb6c1c196bf95352928
Sugguttor <antonofmixa@mail.ru>
2024-06-15 19:20:42 +0300

    KS-3-3

--- src/main/kotlin/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3_task_3.kt
@@ -1,5 +1,7 @@
 fun main() {
-    val numbers: IntArray = intArrayOf(1,2,3,4,5,6,7,8,9,10)
+    val numbers: IntArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val number: Int = 8
-    numbers.forEach {println("$number * $it = ${number * it}")}
+    var s: String = ""
+        numbers.forEach {s+="$number * $it = ${number * it}\n"}
+    println(s)
 }
